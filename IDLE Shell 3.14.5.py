4168065359cabdc5756c306257da1990b6b09de8
Kartikey Pandey <ap3744788@gmail.com>
2026-06-07 14:39:07 +0530

    Add files via upload

---
+++ IDLE Shell 3.14.5.py
@@ -0,0 +1,37 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello,word!")
+Hello,word!
+>>> 2+2
+4
+>>> 2_2
+22
+>>> 2_2
+22
+>>> 2-2
+0
+>>> 21%7
+0
+>>> 21%8
+5
+>>> 2*2*2
+8
+>>> 2**3
+8
+>>> 2**5
+32
+>>> x  = 2
+>>> x+2
+4
+>>> x-2
+0
+>>> x*2
+4
+>>> x%2
+0
+>>> x  =int (input("enter the number:"))
+enter the number:5
+>>> x+10
+15
+>>> x*2
+10
